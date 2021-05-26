e05ced1c13d38583d164f508ced7c3d72446d9f5
and1208es <79553172+and1208es@users.noreply.github.com>
2021-05-25 22:04:25 -0500

    nuevo php

--- enviar.php
+++ enviar.php
@@ -1,14 +1,10 @@
 <?php
-    $destinatario = 'and1208es@gmail.com';
+    $destinatario = "and1208es@gmail.com";
     //este es al correo que llegara el mensaje
-    $nombre = $_POST['nombre'];
-    $mail = $_POST['email'];
-    $mensaje = $_POST['mensaje'];
-
-    $header = "Enviado desde la pagina RURUY" ;
-    $mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
-
-    mail($destinatario, $mensajeCompleto, $header);
-    echo "<script>alert('corre enviado exitosamente'</script>";
-    echo "<script> setTimeout(\"location.href='index.html'\",1000)</script>";
+    $nombre = $_POST["nombre"];
+    $mail = $_POST["email"];
+    $mensaje = $_POST["mensaje"];
+    $contenido = "Nombre: " . $nombre ."\nEmail: " . $email ."\nMensaje: " . $mensaje;
+    mail($destinatario,"Contacto", $contenido);
+    header("location:index.html");
 ?>
